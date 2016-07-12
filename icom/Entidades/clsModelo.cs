a791ce8a3c5cb105d6b5441964e5d2cac5fcd3ff
gerardo_jav_8@hotmail.com <Toluca12>
2016-07-12 16:10:52 -0500

    add clsModelo

---
+++ icom/Entidades/clsModelo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace icom
+{
+	public class clsModelo
+	{
+		public int idmodelo { get; set; }
+		public String nombre { get; set; }
+		public String descripcion { get; set; }
+		public clsModelo()
+		{
+		}
+	}
+}
+
